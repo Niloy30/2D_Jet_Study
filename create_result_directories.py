6440b58fece016ca25c532812ad5697bccbfb166
Niloy30 <niloy.barua@mail.mcgill.ca>
2025-05-19 16:13:43 -0400

    adding create_result_directories.py

---
+++ create_result_directories.py
@@ -0,0 +1,4 @@
+import os
+
+BASE_EXPERIMENT_DIR = r"C:\Users\niloy\Desktop\Experiments\05132025"
+RESULTS_BASE_DIR = r"C:\Users\niloy\Google Drive\School Stuff\M.SC Mechanical Engineering\01 - Fluid Dynamics Lab\03 - PDA\01 - 2D Surface Perturbations\Results"
